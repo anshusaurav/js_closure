8c581d717204136f3a94e2e3516ea519f595d800
anshusaurav <anshu.saurav@gmail.com>
2020-03-13 23:43:14 +0530

    Check

--- higher-order-functions/eloquent/exercise.js
+++ higher-order-functions/eloquent/exercise.js
@@ -1,12 +1,17 @@
 // Challenge 1. Flattening
 let arrays = [[1, 2, 3], [4, 5], [6]];
 
+let res = arrays.reduce((acc, elem) => acc.concat([...elem]));
+console.log(res);
 // Your code here.
 // → [1, 2, 3, 4, 5, 6]
 
 // Challenge 2. Your own loop
 // Your code here.
-
+function loop(n, testFunc, updateFunc, bodyFunc) {
+	for(let i = n; testFunc(i); i = updateFunc(i))
+      bodyFunc(i);
+}
 loop(3, n => n > 0, n => n - 1, console.log);
 // → 3
 // → 2
@@ -14,7 +19,11 @@ loop(3, n => n > 0, n => n - 1, console.log);
 
 // Challenge 3. Everything
 function every(array, test) {
-  // Your code here.
+  return array.reduce((acc, elem) => {
+  	if(!test(elem))
+      acc = false;
+    return acc;
+  }, true);
 }
 
 console.log(every([1, 3, 5], n => n < 10));
@@ -25,9 +34,19 @@ console.log(every([], n => n < 10));
 // → true
 
 // Challenge 4. Dominant writing direction
-function dominantDirection(text) {
-  // Your code here.
-}
+// function dominantDirection(text) {
+//   let scripts = countBy(text, char => {
+//   let script = characterScript(char.codePointAt(0));
+//   return script ? script.direction : "none";
+//   }).filter(({name}) => name != "none");
+
+//   let total = scripts.reduce((n, {count}) => n + count, 0);
+//   if (total == 0) return "No scripts found";
+
+//   return scripts.map(({name, count}) => {
+//     return `${Math.round(count * 100 / total)}% ${name}`;
+//   }).join(", ");
+// }
 
 console.log(dominantDirection("Hello!"));
 // → ltr
