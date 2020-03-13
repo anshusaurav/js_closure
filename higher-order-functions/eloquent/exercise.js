b0dd3d71c955d9b5485697bd01eb6626504f4876
anshusaurav <anshu.saurav@gmail.com>
2020-03-13 23:49:23 +0530

    Check

--- higher-order-functions/eloquent/exercise.js
+++ higher-order-functions/eloquent/exercise.js
@@ -35,17 +35,7 @@ console.log(every([], n => n < 10));
 
 // Challenge 4. Dominant writing direction
 // function dominantDirection(text) {
-//   let scripts = countBy(text, char => {
-//   let script = characterScript(char.codePointAt(0));
-//   return script ? script.direction : "none";
-//   }).filter(({name}) => name != "none");
-
-//   let total = scripts.reduce((n, {count}) => n + count, 0);
-//   if (total == 0) return "No scripts found";
-
-//   return scripts.map(({name, count}) => {
-//     return `${Math.round(count * 100 / total)}% ${name}`;
-//   }).join(", ");
+//   
 // }
 
 console.log(dominantDirection("Hello!"));
